682661c5aee445efa0a786cd1f440cdf1f3ca096
Muhammad Ali <muhammad.alfaqoui@gmail.com>
2025-07-19 18:56:15 +0300

    feat: Implement cart display functionality
    - Add `CartWidget` to display cart items and total price.
    - Integrate `CartWidget` into `CartPage`.
    - Fetch cart items in `CartPage` using `CartCubit`.
    - Handle loading, empty, and error states in `CartWidget`.
    - Calculate and display the total price of items in the cart.

--- lib/feature/cart/presentation/cart/pages/cart_page.dart
+++ lib/feature/cart/presentation/cart/pages/cart_page.dart
@@ -1,19 +1,17 @@
 import '../../../../../global_imports.dart';
+import '../widget/cart_widget.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<CartEntity> cartItems =GoRouterState.of(context).extra as List<CartEntity>;
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Cart')),
-      body:cartItems.isEmpty?
-      Center(
-        child: Text(AppStrings.noDataFound.tr()),
-      ):
-      Container(),
+    return BlocProvider(
+      create: (context) => getIt<CartCubit>()..fetchItems(),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Cart')),
+        body: const CartWidget(),
+      ),
     );
   }
 }
--- lib/feature/cart/presentation/cart/widget/cart_widget.dart
+++ lib/feature/cart/presentation/cart/widget/cart_widget.dart
@@ -1,1 +1,55 @@
+import '../../../../../global_imports.dart';
 
+class CartWidget extends StatelessWidget {
+  const CartWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CartCubit, CartState>(
+      builder: (context, state) {
+        return state.when(
+          initial: () => const SizedBox.shrink(),
+          loading: () => const Center(child: CircularProgressIndicator()),
+          loaded: (items, _, _) {
+            if (items.isEmpty) {
+              return Center(child: Text(AppStrings.noDataFound.tr()));
+            }
+            final total = items.fold<double>(
+              0.0,
+              (sum, item) => sum + item.price,
+            );
+
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      final item = items[index];
+                      return ListTile(
+                        title: Text(item.product.title),
+                        subtitle: Text(item.product.price.toString()),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.grey.shade200,
+                  child: Text(
+                    "${AppStrings.total.tr()}: ${total.toStringAsFixed(2)}",
+                    style: AppTextStyle.style18,
+
+                  ),
+                ),
+              ],
+            );
+          },
+          error: (msg) => Center(child: Text(msg)),
+        );
+      },
+    );
+  }
+}
